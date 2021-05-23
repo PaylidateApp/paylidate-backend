1ff7ac7cf82f0035b5739f3efb990f5a57e6a4e9
Simon Moses Onazi <syflex360@gmail.com>
2021-05-23 16:14:16 +0100

    fixed mails issues

--- app/Mail/RegistrationMail.php
+++ app/Mail/RegistrationMail.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class ResgistrationMail extends Mailable
+class RegistrationMail extends Mailable
 {
     use Queueable, SerializesModels;
     public $user;
