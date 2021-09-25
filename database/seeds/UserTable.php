984a46df465e86d691271e68001169869b9abd43
Simon Onazi Moses <syflex360@gmail.com>
2021-09-25 09:46:30 +0100

    disable all card implementation

--- database/seeds/UserTable.php
+++ database/seeds/UserTable.php
@@ -58,21 +58,21 @@ class UserTable extends Seeder
             'wallet' => 200000
         ]);
 
-         // create NGN virtual card for transactions
-         $virtual_card = $user_virtual_card->virtualCard($currency = 'NGN', $ammount = '150', $name = 'Paylidate Staff');
-         $naira_card_id = null;
+        //  // create NGN virtual card for transactions
+        //  $virtual_card = $user_virtual_card->virtualCard($currency = 'NGN', $ammount = '150', $name = 'Paylidate Staff');
+        //  $naira_card_id = null;
 
-         if ($virtual_card['status'] == 'success') {
-             $naira_card_id = $virtual_card['data']['id'];
-             VirtualCard::create([
-                 'user_id' => 2,
-                 'card_id' => $virtual_card['data']['id'],
-                 'account_id' => $virtual_card['data']['account_id'],
-                 'currency' => $virtual_card['data']['currency'],
-                 'default' => 1,
-             ]);
-         }else {
-             // Mail::raw($virtual_card['message'], function ($message) {
-         }
+        //  if ($virtual_card['status'] == 'success') {
+        //      $naira_card_id = $virtual_card['data']['id'];
+        //      VirtualCard::create([
+        //          'user_id' => 2,
+        //          'card_id' => $virtual_card['data']['id'],
+        //          'account_id' => $virtual_card['data']['account_id'],
+        //          'currency' => $virtual_card['data']['currency'],
+        //          'default' => 1,
+        //      ]);
+        //  }else {
+        //      // Mail::raw($virtual_card['message'], function ($message) {
+        //  }
     }
 }
