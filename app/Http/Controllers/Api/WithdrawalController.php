8b9c88bc9e0b6604c258d306194035eec1d621ba
Philemon Shekari <50109170+philzy94@users.noreply.github.com>
2022-06-16 11:44:33 +0100

    Update WithdrawalController.php

--- app/Http/Controllers/Api/WithdrawalController.php
+++ app/Http/Controllers/Api/WithdrawalController.php
@@ -51,7 +51,7 @@ class WithdrawalController extends Controller
                    
         ]);
 
-     return $reqiest->all();
+     return $request->all();
         try{
             $transaction = Withdrawal::where('transaction_id', $request->transaction_id)->first();
             $payment = Withdrawal::where('payment_id', $request->payment_id)->first();
