ea35a781293539e4ced29b391f802aa5fca5b49c
Philemon Shekari <holyphilzy@gmail.com>
2022-08-16 15:54:56 +0100

    corrected seller email spelling

--- resources/views/mails/report_transaction.blade.php
+++ resources/views/mails/report_transaction.blade.php
@@ -59,7 +59,7 @@
 
     <p style="font-size: 18px"><b>Hello {{$user_name}}</b></p>
     @if ($type == 'report')
-        <p style="font-size: 15px"><b>The transaction with Transaction Ref {{ transation_ref }} was reported by the buyer for "{{$report}}"</b></p>
+        <p style="font-size: 15px"><b>The transaction with Transaction Ref {{ transaction_ref }} was reported by the buyer for "{{$report}}"</b></p>
         <br>
         @if ($user_name == 'Admin' || $user_name == 'Lawrence')
         
@@ -72,14 +72,14 @@
         @endif
 
     @else
-        <p style="font-size: 15px"><b>The transaction with Transaction Ref {{ transation_ref }} that was reported by the buyer for "{{$report}}" has been resolve and transaction process can continue</b></p>
+        <p style="font-size: 15px"><b>The transaction with Transaction Ref {{ transaction_ref }} that was reported by the buyer for "{{$report}}" has been resolve and transaction process can continue</b></p>
         <br>
        
     @endif
 
     
     <p>
-    Click or visit <a href="{{ url('https://www.paylidate.com/escrow-transaction/'.$transation_ref)}}" >https://www.paylidate.com/escrow-transaction/{{$transation_ref}}</a> to view transaction
+    Click or visit <a href="{{ url('https://www.paylidate.com/escrow-transaction/'.$transaction_ref)}}" >https://www.paylidate.com/escrow-transaction/{{$transaction_ref}}</a> to view transaction
     </p>
 
     <p><b>Thanks</b></p>
