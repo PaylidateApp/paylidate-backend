d7602ff28469282b96fffcbc5cad30c2e3ef5365
Philemon Shekari <holyphilzy@gmail.com>
2022-08-24 00:17:49 +0100

    dispute chat

--- app/DisputeChat.php
+++ app/DisputeChat.php
@@ -6,7 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class DisputeChat extends Model
 {
-    
+    protected $fillable = [
+        'user_id','transaction_id','message', 'dispute_id'
+     ];
 
     public function user()
     {
