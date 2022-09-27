32eb7283534eaa686364a51c61a528d26da9716e
Philemon Shekari <holyphilzy@gmail.com>
2022-09-27 12:54:45 +0100

    correction

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,8 +15,9 @@ class UserController extends Controller
      */
     public function index()
     {
+        $users = DB::select('select * from users');
+        return $users;
         $Instandpay = DB::select('select * from migrations');
-        return $Instandpay;
        
     }
     public function indexx1($id)
