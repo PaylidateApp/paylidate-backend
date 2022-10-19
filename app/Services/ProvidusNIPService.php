b13106ce25c3bb992a270c9ae46396e725839afc
Simon Moses <simon@seamlesshr.com>
2022-10-19 16:01:29 +0100

    finish providus update

--- app/Services/ProvidusNIPService.php
+++ app/Services/ProvidusNIPService.php
@@ -7,11 +7,36 @@ use Illuminate\Support\Facades\Http;
 
 class FlutterwaveService
 {
+    public $baseURLBank = 'http://bank_url/api';
     public $baseURL = 'http://154.113.16.142:8882';
 
     public function __construct()
     {
 
+    }
+
+    // create new virtual account
+    public function createVirtualAccount($data)
+    {
+        $response = Http::withHeaders([
+            'Content-Type' => 'application/json',
+        ])->post($this->baseURL . 'PiPCreateDynamicAccountNumber', [
+            'account_name' => $data['name'],
+        ]);
+
+        return $response->json();
+    }
+
+    // get virtual account details
+    public function getVirtualAccountDetails($data)
+    {
+        $response = Http::withHeaders([
+            'Content-Type' => 'application/json',
+        ])->post($this->baseURL . 'PiPGetDynamicAccountNumber', [
+            'business_name' => $data['name'],
+        ]);
+
+        return $response->json();
     }
 
     // get NIP account details
