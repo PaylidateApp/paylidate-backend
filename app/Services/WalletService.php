b21347a49728df6f1c87166229d425af5a32ceb5
Philemon Shekari <holyphilzy@gmail.com>
2022-11-24 20:46:17 +0100

    adding bank name column to wallets table

--- app/Services/WalletService.php
+++ app/Services/WalletService.php
@@ -37,6 +37,7 @@ class WalletService
                 'tx_ref' => $tx_ref,
                 'order_ref' => $accountResponse->order_ref,
                 'account_number' => $accountResponse->account_number,
+                'bank_name' => $accountResponse->bank_name,
             ]);
             return $response;
         } catch (\Exception $e) {
