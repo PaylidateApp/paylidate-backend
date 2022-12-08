994b54fec5af313bb28cb60cce889afc00c9d6fe
David Aremu <segun8427@gmail.com>
2022-12-08 10:39:36 +0100

    Added Notifications count to endpoint

--- app/Http/Controllers/Api/DashboardController.php
+++ app/Http/Controllers/Api/DashboardController.php
@@ -2,14 +2,17 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Dispute;
 use App\Http\Controllers\Controller;
 use App\Payment;
 use Illuminate\Http\Request;
 use App\Product;
 use App\Referer;
+use App\Refund;
 use App\Transaction;
 use App\User;
 use App\Wallet;
+use App\Withdrawal;
 
 class DashboardController extends Controller
 {
@@ -30,6 +33,11 @@ class DashboardController extends Controller
             $balance = Wallet::where('user_id', $user->id)->sum('balance');
             $bonus = Wallet::where('user_id', $user->id)->get('bonus');
 
+            $active_disputes = Dispute::where('user_id', $user->id)->where('dispute_solved', false)->count();
+            $pending_withdrawals = Withdrawal::where('user_id', $user->id)->where('dispute_solved', false)->count();
+            $active_transaction = Transaction::where('user_id', $user->id)->where('status', 0)->count();
+            $pending_refunds = Refund::where('user_id', $user->id)->where('status', false)->count();
+
             return response()->json([
                 'status' => 'success',
                 'message' => 'success',
@@ -38,7 +46,11 @@ class DashboardController extends Controller
                     'payments_made' => $payments_made,
                     'balance' => $bonus,
                     'bonus'   => $bonus,
-                    'refer'   => $referer
+                    'refer'   => $referer,
+                    'active_dsiputes' => $active_disputes,
+                    'pending_withdrawals' => $pending_withdrawals,
+                    'active_transaction' => $active_transaction,
+                    'pending_refunds' => $pending_refunds
                 ]
             ], 200);
 
