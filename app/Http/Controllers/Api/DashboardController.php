f6802883f659b7d01060c7762ea942b22c375fb0
David Aremu <segun8427@gmail.com>
2022-12-09 15:13:38 +0100

    Beta Fix for payment made & payment receive

--- app/Http/Controllers/Api/DashboardController.php
+++ app/Http/Controllers/Api/DashboardController.php
@@ -27,16 +27,46 @@ class DashboardController extends Controller
         }
 
         try {
+
+            // $payments_rec = Transaction::where('user_id', $user->id)->where('status', 1)->with('product')->get();
+            // foreach($payments_rec as $payment){
+            //     $amount=0;
+            //     if($payment->transaction_type == 'sell'){
+            //         $amount += $payment->amount;
+            //         return $amount;
+            //     }
+            //     return $payments_received = $amount;
+            // }
+
+
+            // $payments_m = Transaction::where('user_id', $user->id)->where('status', 1)->with('product')->get();
+            // foreach($payments_m as $payment){
+            //     $amount=0;
+            //     if($payment->transaction_type == 'buy'){
+            //         $amount += $payment->amount;
+            //         return $amount;
+            //     }
+            //     return $payments_made = $amount;
+            // }
+
+
+
+
+            $payments_received = Transaction::where('user_id', $user->id)->where('status', 1)->sum('amount');
             
-            $payments_received = Transaction::where('user_id', $user->id)->sum('amount');
-            $payments_made = Payment::where('user_id', $user->id)->sum('balance_after');
+            $payments_made = Payment::where('user_id', $user->id)->with('transaction')->get();
+            $payments_m = $payments_made->reduce(function ($carry, $item) {
+                    return $carry += empty($item->transaction->amount) ? 0 : $item->transaction->amount;
+                
+            },0);
+
             $referer = Referer::where('user_id', $user->id)->sum('amount');
             $balance = Wallet::where('user_id', $user->id)->get('balance');
             $bonus = Wallet::where('user_id', $user->id)->get('bonus');
 
             //Notification
             $active_disputes = Dispute::where('user_id', $user->id)->where('dispute_solved', false)->count();
-            $pending_withdrawals = Withdrawal::where('user_id', $user->id)->where('dispute_solved', false)->count();
+            $pending_withdrawals = Withdrawal::where('user_id', $user->id)->where('status', false)->count();
             $active_transaction = Transaction::where('user_id', $user->id)->where('status', 0)->count();
             $pending_refunds = Refund::where('user_id', $user->id)->where('status', false)->count();
 
@@ -45,7 +75,7 @@ class DashboardController extends Controller
                 'message' => 'success',
                 'data'    => [
                     'payments_received' => $payments_received,
-                    'payments_made' => $payments_made,
+                    'payments_made' => $payments_m,
                     'balance' => $balance,
                     'bonus'   => $bonus,
                     'refer'   => $referer,
