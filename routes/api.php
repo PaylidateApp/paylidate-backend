0641386a7a5c95b2ff58369785aad61d576a0a0c
David Aremu <segun8427@gmail.com>
2022-12-17 23:20:51 +0100

    Added a static route for fulfilment

--- routes/api.php
+++ routes/api.php
@@ -52,6 +52,7 @@ Route::group(['prefix' => 'api'], function () {
         Route::get('transaction/{T_ref}', 'TransactionController@get_transaction');
 
         // Fulfilment Route
+        Route::get('fulfilment/abcdef', 'FufilmentController@static');
         Route::get('fulfilment/{hash}', 'FufilmentController@get_transaction');
         Route::post('fulfilment/{hash}', 'FufilmentController@confirm_fufilment');
 
