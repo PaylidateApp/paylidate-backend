b9ace7900251fb55aaa5f2deec272778992d6df8
David Aremu <segun8427@gmail.com>
2023-02-03 15:23:15 +0100

    commit -m "Fix updated"

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 echo "Running composer"
+composer update
 composer install --no-dev --working-dir=/var/www/html/
 composer global require hirak/prestissimo
 
-# echo "Caching config..."
-# php artisan config:cache
+echo "Caching config..."
+php artisan config:cache
 
-# echo "Caching routes..."
-# php artisan route:cache
+echo "Caching routes..."
+php artisan route:cache
 
 echo "Running migrations..."
 php artisan migrate --force
